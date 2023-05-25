dca11814647ca10ec4cc0453284a9385599f03a9
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-05-24 19:27:22 -0500

    ADD: router and routes

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,24 +1,34 @@
-import Home from '../Home';
-import MyAccount from '../MyAccount';
-import MyOrders from '../MyOrders';
-import MyOrder from '../MyOrder';
-import NotFound from '../NotFound';
-import SignIn from '../SignIn';
-import './App.css'
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import Home from "../Home";
+import MyAccount from "../MyAccount";
+import MyOrders from "../MyOrders";
+import MyOrder from "../MyOrder";
+import NotFound from "../NotFound";
+import SignIn from "../SignIn";
+import Navbar from "../../Components/Navbar";
+import "./App.css";
 
+// We use useRoutes to create a router for our app.
+const AppRoutes = () => {
+  const routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/my-order/:id", element: <MyOrder /> },
+    { path: "/sign-in", element: <SignIn /> },
+    { path: "*", element: <NotFound /> },
+  ]);
 
-function App() {
-  
+  return routes;
+};
+
+const App = () => {
   return (
-    <div className='bg-red-100'>
-      <Home/>
-      <MyAccount/>
-      <MyOrders/>
-      <MyOrder/>
-      <NotFound/>
-      <SignIn/>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <AppRoutes />
+      <Navbar />
+    </BrowserRouter>
+  );
+};
 
 export default App;
