3e03d59a4ad06c23142cb80081ad8b8a4cef1461
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-05-25 19:00:37 -0500

    ADD: click to show openProductDetail function

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,11 +1,23 @@
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
 import "./styles.css";
 
 const ProductDetail = () => {
+  const context = useContext(ShoppingCartContext);
+  console.log("Product to show: ", context.productToShow);
+
   return (
-    <aside className="product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white">
+    <aside
+      className={`${
+        context.isProductDetailOpen ? "flex" : "hidden"
+      } product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}
+    >
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">Detail</h2>
-        <div>
+        <div
+          className="cursor-pointer"
+          onClick={() => context.closeProductDetail()}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
@@ -16,6 +28,25 @@ const ProductDetail = () => {
           </svg>
         </div>
       </div>
+
+      <figure className="px-6">
+        <img
+          className="w-full h-full rounded-lg"
+          src={context.productToShow.images[0]}
+          alt={context.productToShow.description}
+        />
+      </figure>
+      <p className="flex flex-col p-6">
+        <span className="font-medium text-2xl mb-2">
+          ${context.productToShow.price}
+        </span>
+        <span className="font-medium text-xl">
+          {context.productToShow.title}
+        </span>
+        <span className="font-light text-sm">
+          {context.productToShow.description}
+        </span>
+      </p>
     </aside>
   );
 };
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,12 +4,27 @@ export const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({ children }) => {
   const [count, setCount] = useState(0);
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+  const [productToShow, setProductToShow] = useState([]);
+
+  const openProductDetail = () => {
+    setIsProductDetailOpen(true);
+  };
+
+  const closeProductDetail = () => {
+    setIsProductDetailOpen(false);
+  };
 
   return (
     <ShoppingCartContext.Provider
       value={{
         count,
+        isProductDetailOpen,
+        productToShow,
         setCount,
+        openProductDetail,
+        closeProductDetail,
+        setProductToShow,
       }}
     >
       {children}
