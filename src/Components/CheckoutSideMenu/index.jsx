2ef7bf33c1b69d981142a36f4b8d68f692dea486
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-05-26 22:43:38 -0500

    ADD: CheckoutSideMenu

---
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
+import "./styles.css";
+
+const CheckoutSideMenu = () => {
+  const context = useContext(ShoppingCartContext);
+
+  return (
+    <aside
+      className={`${
+        context.isCheckoutSideMenuOpen ? "flex" : "hidden"
+      } checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}
+    >
+      <div className="flex justify-between items-center p-6">
+        <h2 className="font-medium text-xl">My order</h2>
+        <div
+          className="cursor-pointer"
+          onClick={() => context.closeCheckoutSideMenu()}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-5 h-5"
+          >
+            <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+          </svg>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default CheckoutSideMenu;
