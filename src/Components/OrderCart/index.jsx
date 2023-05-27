4c3f1de7a431c90bcfe005837291d92564d5c8ae
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-05-27 09:23:39 -0500

    ADD: OrderCart component

---
+++ src/Components/OrderCart/index.jsx
@@ -0,0 +1,32 @@
+const OrderCart = (props) => {
+  const { id, title, imageUrl, price, handleDelete } = props;
+
+  return (
+    <div className="flex justify-between items-center mb-3">
+      <div className="flex items-center gap-2">
+        <figure className="w-20 h-20">
+          <img
+            className="w-full h-full rounded-lg object-cover"
+            src={imageUrl}
+            alt={title}
+          />
+        </figure>
+        <p className="text-sm font-light">{title}</p>
+      </div>
+      <div className="flex items-center gap-2">
+        <p className="text-lg font-medium">{price}</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="w-5 h-5"
+          onClick={() => handleDelete(id)}
+        >
+          <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default OrderCart;
