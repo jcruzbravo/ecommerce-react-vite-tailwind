b179fc96a4c8e557d9607981eaa34616f8cc49d2
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-06-03 11:41:11 -0500

    ADD: conditional to show close icon

--- src/Components/OrderCart/index.jsx
+++ src/Components/OrderCart/index.jsx
@@ -1,6 +1,22 @@
 const OrderCart = (props) => {
   const { id, title, imageUrl, price, handleDelete } = props;
 
+  let closeIcon;
+
+  if (handleDelete) {
+    closeIcon = (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="w-5 h-5 cursor-pointer"
+        onClick={() => handleDelete(id)}
+      >
+        <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+      </svg>
+    );
+  }
+
   return (
     <div className="flex justify-between items-center mb-3">
       <div className="flex items-center gap-2">
@@ -15,15 +31,7 @@ const OrderCart = (props) => {
       </div>
       <div className="flex items-center gap-2">
         <p className="text-lg font-medium">${price}</p>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="w-5 h-5"
-          onClick={() => handleDelete(id)}
-        >
-          <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-        </svg>
+        {closeIcon}
       </div>
     </div>
   );
