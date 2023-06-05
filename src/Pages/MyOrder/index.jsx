7ee75ad4ac8eb786a1abfdda4be81a9e4623e517
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-06-04 21:41:27 -0500

    ADD: link to show the list of orders placed

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,16 +1,40 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { ShoppingCartContext } from "../../Context";
 import OrderCart from "../../Components/OrderCart";
 import Layout from "../../Components/Layout";
 
 const MyOrder = () => {
   const context = useContext(ShoppingCartContext);
+  const currentPath = window.location.pathname;
+  let index = currentPath.substring(currentPath.lastIndexOf("/") + 1);
+  if(index === 'last'){
+    index = context.order?.length - 1;
+  }
 
   return (
     <Layout>
-      <h1>My Order</h1>
+      <div className="flex justify-center items-center relative w-80 mb-5">
+        <Link to="/my-orders" className="absolute left-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 text-black cursor-pointer"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5L8.25 12l7.5-7.5"
+            />
+          </svg>
+        </Link>
+        <h1>My Order</h1>
+      </div>
       <div className="flex flex-col w-80">
-        {context.order?.slice(-1)[0].products.map((product) => (
+        {context.order?.[index]?.products.map((product) => (
           <OrderCart
             key={product.id}
             id={product.id}
