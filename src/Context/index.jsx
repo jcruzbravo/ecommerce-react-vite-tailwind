d6c8edf93b3db4da80592595ed4f0a65c17ac959
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-06-04 23:08:54 -0500

    ADD: functions to filter

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const ShoppingCartContext = createContext();
 
@@ -9,6 +9,71 @@ export const ShoppingCartProvider = ({ children }) => {
   const [cartProducts, setCartProducts] = useState([]);
   const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
   const [order, setOrder] = useState([]);
+  const [items, setItems] = useState(null);
+  const [filteredItems, setFilteredItems] = useState(null);
+  const [searchByTitle, setSearchByTitle] = useState(null);
+  const [searchByCategory, setSearchByCategory] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
+
+  const filterItemsByTitle = (items, searchByTitle) => {
+    return items?.filter((item) =>
+      item.title.toLowerCase().includes(searchByTitle.toLowerCase())
+    );
+  };
+
+  const filterItemsByCategory = (items, searchByCategory) => {
+    return items?.filter((item) =>
+      item.category.name.toLowerCase().includes(searchByCategory.toLowerCase())
+    );
+  };
+
+  const filterBy = (searchType, items, searchByTitle, searchByCategory) => {
+    if (searchType === "BY_TITLE") {
+      return filterItemsByTitle(items, searchByTitle);
+    }
+
+    if (searchType === "BY_CATEGORY") {
+      return filterItemsByCategory(items, searchByCategory);
+    }
+
+    if (searchType === "BY_TITLE_AND_CATEGORY") {
+      return filterItemsByCategory(items, searchByCategory).filter((item) =>
+        item.title.toLowerCase().includes(searchByTitle.toLowerCase())
+      );
+    }
+
+    if (!searchType) {
+      return items;
+    }
+
+  };
+
+  useEffect(() => {
+    if (searchByTitle && searchByCategory)
+      setFilteredItems(
+        filterBy(
+          "BY_TITLE_AND_CATEGORY",
+          items,
+          searchByTitle,
+          searchByCategory
+        )
+      );
+    if (searchByTitle && !searchByCategory)
+      setFilteredItems(
+        filterBy("BY_TITLE", items, searchByTitle, searchByCategory)
+      );
+    if (!searchByTitle && searchByCategory)
+      setFilteredItems(
+        filterBy("BY_CATEGORY", items, searchByTitle, searchByCategory)
+      );
+    if (!searchByTitle && !searchByCategory)
+      setFilteredItems(filterBy(null, items, searchByTitle, searchByCategory));
+  }, [items, searchByTitle, searchByCategory]);
 
   const openProductDetail = () => {
     setIsProductDetailOpen(true);
@@ -35,6 +100,10 @@ export const ShoppingCartProvider = ({ children }) => {
         cartProducts,
         isCheckoutSideMenuOpen,
         order,
+        items,
+        searchByTitle,
+        filteredItems,
+        searchByCategory,
         setCount,
         openProductDetail,
         closeProductDetail,
@@ -42,7 +111,10 @@ export const ShoppingCartProvider = ({ children }) => {
         setCartProducts,
         openCheckoutSideMenu,
         closeCheckoutSideMenu,
-        setOrder
+        setOrder,
+        setItems,
+        setSearchByTitle,
+        setSearchByCategory,
       }}
     >
       {children}
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,22 +1,36 @@
-import { useState, useEffect } from "react";
+import { useContext } from "react";
 import Layout from "../../Components/Layout";
+import { ShoppingCartContext } from "../../Context";
 import Card from "../../Components/Card";
 import ProductDetail from "../../Components/ProductDetail";
 
 const Home = () => {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    fetch("https://api.escuelajs.co/api/v1/products")
-      .then((res) => res.json())
-      .then((data) => setItems(data));
-  }, []);
+  const context = useContext(ShoppingCartContext);
+  const renderView = () => {
+    if (context.searchByTitle?.length > 0) {
+      if (context.filteredItems?.length > 0) {
+        return context.filteredItems?.map((item) => <Card key={item.id} data={item} />);
+      } else {
+        return <h2>Products not found ☹️</h2>
+      }
+    } else {
+      return context.items?.map((item) => <Card key={item.id} data={item} />);
+    }
+  };
 
   return (
     <Layout>
-      Home
+      <div className="flex flex-col items-center gap-4 justify-center mb-5">
+        <h1 className="font-medium text-xl">Exclusive products</h1>
+        <input
+          className="border-2 border-black rounded-lg w-80 p-4 focus:outline-none"
+          type="text"
+          placeholder="Search a product"
+          onChange={(e) => context.setSearchByTitle(e.target.value)}
+        />
+      </div>
       <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
-        {items?.map((item) => [<Card key={item.id} data={item} />])}
+        {renderView()}
       </div>
       <ProductDetail />
     </Layout>
