302519bb1e1c9c50d14530a026276c3cdc9499f2
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-06-06 21:06:06 -0500

    ADD: margin bottom

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,7 +1,7 @@
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col items-center mt-20">
+    <div className="flex flex-col items-center mt-20 mb-5">
       {children}
     </div>
   )
